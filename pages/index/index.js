e4eeec20187fbea0a11f292027e94bff0e984ce6
eddy20001118 <1416964247@qq.com>
2018-12-23 16:43:45 +0000

    新增队伍详情页，赛事详情页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,26 @@
 Page({
     data : {
-        list : ["a","b",'c','d','e','f']
+        teamInfo: { 
+            teamNumber : "6766",
+            teamName : "Pharma Atom Storm",
+            teamLocation: "Shenzhen, Guangdong, China"
+        },
+        eventInfo: {
+            eventTitle: "Shenzhen Regional",
+            eventLocation: "Shenzhen Shi, Guangdong Sheng, China",
+            eventStartDate: "Mar 7",
+            eventEndDate: "Mar 10",
+            eventYear: "2019"
+        }
+    },
+    onEventCardClick:function(){
+        wx.navigateTo({
+            url: "/pages/eventDetail/eventDetail?eventName="+this.data.eventInfo.eventTitle,
+        })
+    },
+    onTeamCardClick: function () {
+        wx.navigateTo({
+            url: '/pages/teamDetail/teamDetail?teamNumber='+this.data.teamInfo.teamNumber
+        })
     }
 })
